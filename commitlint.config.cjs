96696b0808a5641631627aca9450d8b9059b82a1
David Worms <david@adaltas.com>
2023-05-29 16:03:08 +0200

    build(engine): restrictive commit scopes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,22 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", ["mdx", "engine"]],
+    "scope-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "revert",
+        "test",
+      ],
+    ],
+  },
 };
