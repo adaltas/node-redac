ab12e15e9dbb7ffbee2a49db6a2f34138f2c52d6
David Worms <david@adaltas.com>
2023-06-03 09:21:31 +0200

    feat(engine): plugin module as a function and an object

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import {describe, it} from 'node:test'
+import { describe, it } from 'node:test'
 import 'should'
 import engine from '../lib/index.js'
 
@@ -7,5 +7,55 @@ describe('engine', async () => {
     const eng = await engine()
     Object.keys(eng).should.eql(['db', 'from'])
   })
-
+  it('config.module may be a function', async () => {
+    await engine([
+      {
+        module: ({config}) => ({
+          source: () => config.documents,
+        }),
+        config: {
+          documents: [
+            {
+              collection: 'blog',
+              slug: ['article'],
+              lang: 'en',
+            },
+            {
+              collection: 'blog',
+              slug: ['article'],
+              lang: 'fr',
+            },
+          ],
+        },
+      },
+    ])
+      .from('blog')
+      .list()
+      .map(({ lang }) => lang)
+      .should.be.resolvedWith(['en', 'fr'])
+  })
+  it('config.module may be an object', async () => {
+    await engine([
+      {
+        module: {
+          source: () => [
+            {
+              collection: 'blog',
+              slug: ['article'],
+              lang: 'en',
+            },
+            {
+              collection: 'blog',
+              slug: ['article'],
+              lang: 'fr',
+            },
+          ],
+        },
+      },
+    ])
+      .from('blog')
+      .list()
+      .map(({ lang }) => lang)
+      .should.be.resolvedWith(['en', 'fr'])
+  })
 })
